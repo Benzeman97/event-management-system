d3fd1ab862d9d3ec6b8b816443687ecc17c366ee
Nafaz M N M <nafazbenzema@gmail.com>
2026-01-08 17:28:14 +0530

    Update EventMapper.java
    EventMapper implemeted

---
+++ event-management-service/src/main/java/com/event/api/mapper/EventMapper.java
@@ -0,0 +1,45 @@
+package com.event.api.mapper;
+
+import com.event.api.entity.Event;
+
+public class EventMapper{
+
+
+    public static Event toEntity(CreateEventRequest request, User host){
+      
+             LocalDateTime startTime = DateTimeUtil.parseToLocalDateTime(request.getStartTime());
+             LocalDateTime endTime = DateTimeUtil.parseToLocalDateTime(request.getEndTime());
+
+             Event event = new Event();
+             event.setTitle(request.getTitle());
+             event.setDescription(request.getDescription());
+             event.setLocation(request.getLocation());
+             event.setStartTime(DateTimeUtil.convertLocalDateTimeToInstant(startTime));
+             event.setEndTime(DateTimeUtil.convertLocalDateTimeToInstant(endTime));
+             event.setVisibility(EventVisibilityType.valueOf(request.getEventVisibilityType().toUpperCase()));
+             event.setHost(host);
+      
+             event.getAttendances().add(handleHostAttendance(event,host));
+
+            return event;
+    }
+
+     public static Event toEntity(UpdateEventRequest request,User host, Event event) {
+       
+             LocalDateTime startTime = DateTimeUtil.parseToLocalDateTime(request.getStartTime());
+             LocalDateTime endTime = DateTimeUtil.parseToLocalDateTime(request.getEndTime());
+
+            event.setTitle(request.getTitle());
+            event.setDescription(request.getDescription());
+            event.setLocation(request.getLocation());
+            event.setStartTime(DateTimeUtil.convertLocalDateTimeToInstant(startTime));
+            event.setEndTime(DateTimeUtil.convertLocalDateTimeToInstant(endTime));
+            event.setVisibility(EventVisibilityType.valueOf(request.getEventVisibilityType().toUpperCase()));
+            event.setHost(host);
+       
+            event.getAttendances().add(handleHostAttendance(event,host));
+            return event;
+       
+     }
+
+}
