1fa2b78887e3ad0cfb2067a2fe61036fa65a0cf4
Nafaz M N M <nafazbenzema@gmail.com>
2026-01-08 17:34:54 +0530

    Update EventMapper

--- event-management-service/src/main/java/com/event/api/mapper/EventMapper.java
+++ event-management-service/src/main/java/com/event/api/mapper/EventMapper.java
@@ -5,7 +5,7 @@ import com.event.api.entity.Event;
 public class EventMapper{
 
 
-    public static Event toEntity(CreateEventRequest request, User host){
+    public static Event createEventFromRequest(CreateEventRequest request, User host){
       
              LocalDateTime startTime = DateTimeUtil.parseToLocalDateTime(request.getStartTime());
              LocalDateTime endTime = DateTimeUtil.parseToLocalDateTime(request.getEndTime());
@@ -24,7 +24,7 @@ public class EventMapper{
             return event;
     }
 
-     public static Event toEntity(UpdateEventRequest request,User host, Event event) {
+     public static Event updateEventFromRequest(UpdateEventRequest request,User host, Event event) {
        
              LocalDateTime startTime = DateTimeUtil.parseToLocalDateTime(request.getStartTime());
              LocalDateTime endTime = DateTimeUtil.parseToLocalDateTime(request.getEndTime());
@@ -43,3 +43,4 @@ public class EventMapper{
      }
 
 }
+
