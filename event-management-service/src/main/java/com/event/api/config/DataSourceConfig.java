19ae1b7cb44be50a55f5e6262f3da6b086ef864f
Nafaz M N M <nafazbenzema@gmail.com>
2026-01-11 13:03:46 +0530

    Update DataSourceConfig.java

--- event-management-service/src/main/java/com/event/api/config/DataSourceConfig.java
+++ event-management-service/src/main/java/com/event/api/config/DataSourceConfig.java
@@ -1,1 +1,44 @@
 
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.master")
+    public DataSource masterDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+     @Bean
+     @ConfigurationProperties(prefix = "spring.datasource.slave1")
+     public DataSource slave1DataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.slave2")
+    public DataSource slave2DataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    public DataSource routingDataSource(DataSource masterDataSource,
+                                        DataSource slave1DataSource,
+                                        DataSource slave2DataSource) {
+      
+      Map<Object, Object> targetDataSources = new HashMap<>();
+      targetDataSources.put("MASTER", masterDataSource);
+      targetDataSources.put("SLAVE1", slave1DataSource);
+      targetDataSources.put("SLAVE2", slave2DataSource);
+
+      ReplicationRoutingDataSource routingDataSource = new ReplicationRoutingDataSource();
+      routingDataSource.setTargetDataSources(targetDataSources);
+      routingDataSource.setDefaultTargetDataSource(masterDataSource);
+      return routingDataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+}
