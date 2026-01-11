dc9277e180b210737f253fd60936e813724497d0
Nafaz M N M <nafazbenzema@gmail.com>
2026-01-11 17:26:01 +0530

    Update ReplicationRoutingDataSource.java

--- event-management-service/src/main/java/com/event/api/config/ReplicationRoutingDataSource.java
+++ event-management-service/src/main/java/com/event/api/config/ReplicationRoutingDataSource.java
@@ -4,15 +4,17 @@ public class ReplicationRoutingDataSource extends AbstractRoutingDataSource {
 
       private final Random random = new Random();
 
+      // Every time a database connection is requested, Spring calls this method to decide which actual DataSource (MASTER or SLAVE) to use.
+
     @Override
     protected Object determineCurrentLookupKey() {
 
         //Read-only transactions → randomly go to SLAVE1 or SLAVE2
         if (ReplicationContext.isReadOnly()) {
             // Randomly choose one of the two slaves
-            return random.nextBoolean() ? "SLAVE1" : "SLAVE2";
+            return random.nextBoolean() ? DataSourceType.SLAVE1 : DataSourceType.SLAVE2;
         } else {
-            return "MASTER";
+            return DataSourceType.MASTER;
             // Writes → always go to MASTER
         }
     }
