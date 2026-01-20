0659754176c7b018ddcd5a7fa5f2c529ef0e94f6
Nafaz M N M <nafazbenzema@gmail.com>
2026-01-20 12:44:27 +0530

    Update FeignErrorDecoder.java

--- event-management-service/src/main/java/com/event/api/client/feign/FeignErrorDecoder.java
+++ event-management-service/src/main/java/com/event/api/client/feign/FeignErrorDecoder.java
@@ -34,4 +34,19 @@ public class FeignErrorDecoder implements ErrorDecoder {
                 return new RuntimeException(message);
         }
     }
+
+   /* @Override
+    public Exception decode(String methodKey, Response response) {
+        switch (response.status()) {
+            case 400:
+                return new IllegalArgumentException("Bad request from " + methodKey);
+            case 404:
+                return new RuntimeException("Resource not found in " + methodKey);
+            case 500:
+                return new RuntimeException("Internal server error from " + methodKey);
+            default:
+                return new RuntimeException("Feign error: " + response.status());
+        }
+    } */
 }
+
