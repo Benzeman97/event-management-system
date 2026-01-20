22151f46458bf07ee54e5fbf42a8c4f596417f1b
Nafaz M N M <nafazbenzema@gmail.com>
2026-01-20 12:36:08 +0530

    Update FeignErrorDecoder.java

--- event-management-service/src/main/java/com/event/api/client/feign/FeignErrorDecoder.java
+++ event-management-service/src/main/java/com/event/api/client/feign/FeignErrorDecoder.java
@@ -1,18 +1,37 @@
+package com.example.orderservice.exception;
+
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.springframework.stereotype.Component;
+
 @Component
 public class FeignErrorDecoder implements ErrorDecoder {
 
     @Override
     public Exception decode(String methodKey, Response response) {
-        
+
+        String message = "Feign error in " + methodKey + " with status " + response.status();
+
         switch (response.status()) {
             case 400:
-                return new IllegalArgumentException("Bad request from " + methodKey);
+                return new IllegalArgumentException("Bad request: " + message);
             case 404:
-                return new RuntimeException("Resource not found in " + methodKey);
+                if (methodKey.contains("UserClient")) {
+                    return new UserServiceException(message);
+                } else if (methodKey.contains("PaymentClient")) {
+                    return new PaymentServiceException(message);
+                } else if (methodKey.contains("BookingClient")) {
+                    return new BookingServiceException(message);
+                } else {
+                    return new RuntimeException(message);
+                }
             case 500:
-                return new RuntimeException("Internal server error from " + methodKey);
+                if (methodKey.contains("PaymentClient")) {
+                    return new PaymentServiceException(message);
+                }
+                return new RuntimeException("Internal server error: " + message);
             default:
-                return new RuntimeException("Feign error: " + response.status());
+                return new RuntimeException(message);
         }
     }
 }
