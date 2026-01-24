28a3d8f3ad56ed984bb1a12271d3f059f6d0bfc4
Nafaz M N M <nafazbenzema@gmail.com>
2026-01-24 17:38:06 +0530

    Update UserController.java

--- event-management-service/src/main/java/com/event/api/controller/UserController.java
+++ event-management-service/src/main/java/com/event/api/controller/UserController.java
@@ -1,1 +1,18 @@
 
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+     private final UserController userController;
+
+     public UserController(UserController userController){
+            this.userController = userController;
+     }
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<UserResponse> getUser(@PathVariable int userId){
+          return ResponseEntity.ok(patientService.getPatientById(id));
+    }
+
+    
+}
