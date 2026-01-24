beed38a69d18a04008bb327b96fae114cba2c7b0
Nafaz M N M <nafazbenzema@gmail.com>
2026-01-24 23:44:39 +0530

    Create ExecutorConfig.java

---
+++ event-management-service/src/main/java/com/event/api/config/ExecutorConfig.java
@@ -0,0 +1,16 @@
+@Configuration
+public class ExecutorConfig {
+    
+    @Bean(name = "taskExecutor")
+    public ThreadPoolTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("async-");
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        executor.setAwaitTerminationSeconds(60);
+        executor.initialize();
+        return executor;
+    }
+}
