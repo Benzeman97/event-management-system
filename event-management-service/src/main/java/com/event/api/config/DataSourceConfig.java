b9fd1246f00ca41e1b8aa358a5370b2863807006
Nafaz M N M <nafazbenzema@gmail.com>
2026-01-25 12:56:33 +0530

    Update DataSourceConfig.java

--- event-management-service/src/main/java/com/event/api/config/DataSourceConfig.java
+++ event-management-service/src/main/java/com/event/api/config/DataSourceConfig.java
@@ -3,44 +3,59 @@
 @Configuration
 public class DataSourceConfig {
 
+    // ---------- MASTER ----------
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.master")
-    public DataSource masterDataSource() {
-        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    @ConfigurationProperties("spring.datasource.master.hikari")
+    public HikariDataSource masterDataSource() {
+        return new HikariDataSource();
     }
 
-     @Bean
-     @ConfigurationProperties(prefix = "spring.datasource.slave1")
-     public DataSource slave1DataSource() {
-        return DataSourceBuilder.create().type(HikariDataSource.class).build();
-    }
-
+    // ---------- SLAVE 1 ----------
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.slave2")
-    public DataSource slave2DataSource() {
-        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    @ConfigurationProperties("spring.datasource.slave1.hikari")
+    public HikariDataSource slave1DataSource() {
+        return new HikariDataSource();
     }
 
+    // ---------- SLAVE 2 ----------
+    @Bean
+    @ConfigurationProperties("spring.datasource.slave2.hikari")
+    public HikariDataSource slave2DataSource() {
+        return new HikariDataSource();
+    }
+
+    // ---------- ROUTING ----------
+    @Bean
+    public DataSource routingDataSource(
+            @Qualifier("masterDataSource") DataSource master,
+            @Qualifier("slave1DataSource") DataSource slave1,
+            @Qualifier("slave2DataSource") DataSource slave2) {
+
+        Map<Object, Object> targets = new HashMap<>();
+        targets.put(DataSourceType.MASTER, master);
+        targets.put(DataSourceType.SLAVE1, slave1);
+        targets.put(DataSourceType.SLAVE2, slave2);
+
+        ReplicationRoutingDataSource routing = new ReplicationRoutingDataSource();
+        routing.setTargetDataSources(targets);
+        routing.setDefaultTargetDataSource(master);
+        routing.afterPropertiesSet();
+
+        return routing;
+    }
+
+    // ---------- IMPORTANT ----------
+    // Prevents early connection selection
     @Bean
     @Primary
-    public DataSource routingDataSource(DataSource masterDataSource,
-                                        DataSource slave1DataSource,
-                                        DataSource slave2DataSource) {
-      
-      Map<Object, Object> targetDataSources = new HashMap<>();
-      targetDataSources.put(DataSourceType.MASTER, masterDataSource);
-      targetDataSources.put(DataSourceType.SLAVE1, slave1DataSource);
-      targetDataSources.put(DataSourceType.SLAVE2, slave2DataSource);
-
-      ReplicationRoutingDataSource routingDataSource = new ReplicationRoutingDataSource();
-      routingDataSource.setTargetDataSources(targetDataSources);
-      routingDataSource.setDefaultTargetDataSource(masterDataSource);
-      routingDataSource.afterPropertiesSet();
-      return routingDataSource;
+    public DataSource dataSource(DataSource routingDataSource) {
+        return new LazyConnectionDataSourceProxy(routingDataSource);
     }
 
+    // ---------- TX MANAGER ----------
     @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+    public PlatformTransactionManager transactionManager(
+            EntityManagerFactory emf) {
         return new JpaTransactionManager(emf);
     }
 }
