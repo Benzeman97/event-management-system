a3f56fca638cd60803661c8afd0d451185e84279
Nafaz M N M <nafazbenzema@gmail.com>
2026-01-25 13:16:35 +0530

    Update ReplicationRoutingDataSource.java

--- event-management-service/src/main/java/com/event/api/config/ReplicationRoutingDataSource.java
+++ event-management-service/src/main/java/com/event/api/config/ReplicationRoutingDataSource.java
@@ -2,21 +2,35 @@
 
 public class ReplicationRoutingDataSource extends AbstractRoutingDataSource {
 
+          // List of slaves
+    private static final List<DataSourceType> SLAVES = List.of(
+            DataSourceType.SLAVE1,
+            DataSourceType.SLAVE2
+    );
+
+      // ThreadLocal to stick a slave per transaction/thread
+    private static final ThreadLocal<DataSourceType> SELECTED_SLAVE = new ThreadLocal<>();
+
       private final Random random = new Random();
 
       // Every time a database connection is requested, Spring calls this method to decide which actual DataSource (MASTER or SLAVE) to use.
 
-    @Override
+     @Override
     protected Object determineCurrentLookupKey() {
 
-        //Read-only transactions → randomly go to SLAVE1 or SLAVE2
-        if (ReplicationContext.isReadOnly()) {
-            // Randomly choose one of the two slaves
-            return random.nextBoolean() ? DataSourceType.SLAVE1 : DataSourceType.SLAVE2;
-        } else {
+        // Writes → always go to MASTER
+        if (!ReplicationContext.isReadOnly()) {
+            SELECTED_SLAVE.remove();  // Remove sticky slave for write transaction
             return DataSourceType.MASTER;
-            // Writes → always go to MASTER
         }
+
+        // Reads → pick one slave per transaction/thread
+        if (SELECTED_SLAVE.get() == null) {
+            SELECTED_SLAVE.set(SLAVES.get(random.nextInt(SLAVES.size())));
+        }
+
+        return SELECTED_SLAVE.get();
     }
+
 
 }
